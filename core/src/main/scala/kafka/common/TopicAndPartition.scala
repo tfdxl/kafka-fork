08e9592289176b19220ba83e3a457c4797f71594
tianfeng <1114089877@qq.com>
2018-07-02 23:10:08 +0800

    feat:add comment

--- core/src/main/scala/kafka/common/TopicAndPartition.scala
+++ core/src/main/scala/kafka/common/TopicAndPartition.scala
@@ -3,28 +3,36 @@ package kafka.common
 import org.apache.kafka.common.TopicPartition
 
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * Licensed to the Apache Software Foundation (ASF) under one or more
+  * contributor license agreements.  See the NOTICE file distributed with
+  * this work for additional information regarding copyright ownership.
+  * The ASF licenses this file to You under the Apache License, Version 2.0
+  * (the "License"); you may not use this file except in compliance with
+  * the License.  You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 /**
- * Convenience case class since (topic, partition) pairs are ubiquitous.
- */
+  * Convenience case class since (topic, partition) pairs are ubiquitous.
+  */
 case class TopicAndPartition(topic: String, partition: Int) {
 
+  //构造方法
   def this(topicPartition: TopicPartition) = this(topicPartition.topic, topicPartition.partition)
 
   override def toString: String = s"$topic-$partition"
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val topicAndPartition = new TopicAndPartition(s"topic", 789)
+    println(topicAndPartition)
+  }
 }
