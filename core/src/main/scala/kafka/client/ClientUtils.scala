301b79897a77d1a8ad11932798bd9b5bd19316fb
tianfeng <1114089877@qq.com>
2019-04-25 22:01:20 +0800

    feat:消除阿里巴巴的警告，垃圾警告

--- core/src/main/scala/kafka/client/ClientUtils.scala
+++ core/src/main/scala/kafka/client/ClientUtils.scala
@@ -1,19 +1,19 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+  * Licensed to the Apache Software Foundation (ASF) under one or more
+  * contributor license agreements.  See the NOTICE file distributed with
+  * this work for additional information regarding copyright ownership.
+  * The ASF licenses this file to You under the Apache License, Version 2.0
+  * (the "License"); you may not use this file except in compliance with
+  * the License.  You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package kafka.client
 
 import org.apache.kafka.common.protocol.Errors
@@ -33,19 +33,20 @@ import java.io.IOException
 
 import org.apache.kafka.common.security.auth.SecurityProtocol
 
- /**
- * Helper functions common to clients (producer, consumer, or admin)
- */
+/**
+  * Helper functions common to clients (producer, consumer, or admin)
+  */
 @deprecated("This class has been deprecated and will be removed in a future release.", "0.11.0.0")
 object ClientUtils extends Logging {
 
   /**
-   * Used by the producer to send a metadata request since it has access to the ProducerConfig
-   * @param topics The topics for which the metadata needs to be fetched
-   * @param brokers The brokers in the cluster as configured on the producer through metadata.broker.list
-   * @param producerConfig The producer's config
-   * @return topic metadata response
-   */
+    * Used by the producer to send a metadata request since it has access to the ProducerConfig
+    *
+    * @param topics         The topics for which the metadata needs to be fetched
+    * @param brokers        The brokers in the cluster as configured on the producer through metadata.broker.list
+    * @param producerConfig The producer's config
+    * @return topic metadata response
+    */
   @deprecated("This method has been deprecated and will be removed in a future release.", "0.10.0.0")
   def fetchTopicMetadata(topics: Set[String], brokers: Seq[BrokerEndPoint], producerConfig: ProducerConfig, correlationId: Int): TopicMetadataResponse = {
     var fetchMetaDataSucceeded: Boolean = false
@@ -56,7 +57,7 @@ object ClientUtils extends Logging {
     // shuffle the list of brokers before sending metadata requests so that most requests don't get routed to the
     // same broker
     val shuffledBrokers = Random.shuffle(brokers)
-    while(i < shuffledBrokers.size && !fetchMetaDataSucceeded) {
+    while (i < shuffledBrokers.size && !fetchMetaDataSucceeded) {
       val producer: SyncProducer = ProducerPool.createSyncProducer(producerConfig, shuffledBrokers(i))
       info("Fetching metadata from broker %s with correlation id %d for %d topic(s) %s".format(shuffledBrokers(i), correlationId, topics.size, topics))
       try {
@@ -73,7 +74,7 @@ object ClientUtils extends Logging {
         producer.close()
       }
     }
-    if(!fetchMetaDataSucceeded) {
+    if (!fetchMetaDataSucceeded) {
       throw new KafkaException("fetching topic metadata for topics [%s] from broker [%s] failed".format(topics, shuffledBrokers), t)
     } else {
       debug("Successfully fetched metadata for %d topic(s) %s".format(topics.size, topics))
@@ -82,12 +83,13 @@ object ClientUtils extends Logging {
   }
 
   /**
-   * Used by a non-producer client to send a metadata request
-   * @param topics The topics for which the metadata needs to be fetched
-   * @param brokers The brokers in the cluster as configured on the client
-   * @param clientId The client's identifier
-   * @return topic metadata response
-   */
+    * Used by a non-producer client to send a metadata request
+    *
+    * @param topics   The topics for which the metadata needs to be fetched
+    * @param brokers  The brokers in the cluster as configured on the client
+    * @param clientId The client's identifier
+    * @return topic metadata response
+    */
   def fetchTopicMetadata(topics: Set[String], brokers: Seq[BrokerEndPoint], clientId: String, timeoutMs: Int,
                          correlationId: Int = 0): TopicMetadataResponse = {
     val props = new Properties()
@@ -99,8 +101,8 @@ object ClientUtils extends Logging {
   }
 
   /**
-   * Parse a list of broker urls in the form host1:port1, host2:port2, ...
-   */
+    * Parse a list of broker urls in the form host1:port1, host2:port2, ...
+    */
   def parseBrokerList(brokerListStr: String): Seq[BrokerEndPoint] = {
     val brokersStr = CoreUtils.parseCsvList(brokerListStr)
 
@@ -110,9 +112,9 @@ object ClientUtils extends Logging {
   }
 
   /**
-   * Creates a blocking channel to a random broker
-   */
-  def channelToAnyBroker(zkUtils: ZkUtils, socketTimeoutMs: Int = 3000) : BlockingChannel = {
+    * Creates a blocking channel to a random broker
+    */
+  def channelToAnyBroker(zkUtils: ZkUtils, socketTimeoutMs: Int = 3000): BlockingChannel = {
     var channel: BlockingChannel = null
     var connected = false
     while (!connected) {
@@ -138,7 +140,7 @@ object ClientUtils extends Logging {
     channel
   }
 
-   /**
+  /**
     * Returns the first end point from each broker with the PLAINTEXT security protocol.
     */
   def getPlaintextBrokerEndPoints(zkUtils: ZkUtils): Seq[BrokerEndPoint] = {
@@ -150,68 +152,68 @@ object ClientUtils extends Logging {
     }
   }
 
-   /**
+  /**
     * Creates a blocking channel to the offset manager of the given group
     */
-   def channelToOffsetManager(group: String, zkUtils: ZkUtils, socketTimeoutMs: Int = 3000, retryBackOffMs: Int = 1000) = {
-     var queryChannel = channelToAnyBroker(zkUtils)
+  def channelToOffsetManager(group: String, zkUtils: ZkUtils, socketTimeoutMs: Int = 3000, retryBackOffMs: Int = 1000) = {
+    var queryChannel = channelToAnyBroker(zkUtils)
 
-     var offsetManagerChannelOpt: Option[BlockingChannel] = None
+    var offsetManagerChannelOpt: Option[BlockingChannel] = None
 
-     while (offsetManagerChannelOpt.isEmpty) {
+    while (offsetManagerChannelOpt.isEmpty) {
 
-       var coordinatorOpt: Option[BrokerEndPoint] = None
+      var coordinatorOpt: Option[BrokerEndPoint] = None
 
-       while (coordinatorOpt.isEmpty) {
-         try {
-           if (!queryChannel.isConnected)
-             queryChannel = channelToAnyBroker(zkUtils)
-           debug("Querying %s:%d to locate offset manager for %s.".format(queryChannel.host, queryChannel.port, group))
-           queryChannel.send(GroupCoordinatorRequest(group))
-           val response = queryChannel.receive()
-           val consumerMetadataResponse =  GroupCoordinatorResponse.readFrom(response.payload())
-           debug("Consumer metadata response: " + consumerMetadataResponse.toString)
-           if (consumerMetadataResponse.error == Errors.NONE)
-             coordinatorOpt = consumerMetadataResponse.coordinatorOpt
-           else {
-             debug("Query to %s:%d to locate offset manager for %s failed - will retry in %d milliseconds."
-                  .format(queryChannel.host, queryChannel.port, group, retryBackOffMs))
-             Thread.sleep(retryBackOffMs)
-           }
-         }
-         catch {
-           case _: IOException =>
-             info("Failed to fetch consumer metadata from %s:%d.".format(queryChannel.host, queryChannel.port))
-             queryChannel.disconnect()
-         }
-       }
+      while (coordinatorOpt.isEmpty) {
+        try {
+          if (!queryChannel.isConnected)
+            queryChannel = channelToAnyBroker(zkUtils)
+          debug("Querying %s:%d to locate offset manager for %s.".format(queryChannel.host, queryChannel.port, group))
+          queryChannel.send(GroupCoordinatorRequest(group))
+          val response = queryChannel.receive()
+          val consumerMetadataResponse = GroupCoordinatorResponse.readFrom(response.payload())
+          debug("Consumer metadata response: " + consumerMetadataResponse.toString)
+          if (consumerMetadataResponse.error == Errors.NONE)
+            coordinatorOpt = consumerMetadataResponse.coordinatorOpt
+          else {
+            debug("Query to %s:%d to locate offset manager for %s failed - will retry in %d milliseconds."
+              .format(queryChannel.host, queryChannel.port, group, retryBackOffMs))
+            Thread.sleep(retryBackOffMs)
+          }
+        }
+        catch {
+          case _: IOException =>
+            info("Failed to fetch consumer metadata from %s:%d.".format(queryChannel.host, queryChannel.port))
+            queryChannel.disconnect()
+        }
+      }
 
-       val coordinator = coordinatorOpt.get
-       if (coordinator.host == queryChannel.host && coordinator.port == queryChannel.port) {
-         offsetManagerChannelOpt = Some(queryChannel)
-       } else {
-         val connectString = "%s:%d".format(coordinator.host, coordinator.port)
-         var offsetManagerChannel: BlockingChannel = null
-         try {
-           debug("Connecting to offset manager %s.".format(connectString))
-           offsetManagerChannel = new BlockingChannel(coordinator.host, coordinator.port,
-                                                      BlockingChannel.UseDefaultBufferSize,
-                                                      BlockingChannel.UseDefaultBufferSize,
-                                                      socketTimeoutMs)
-           offsetManagerChannel.connect()
-           offsetManagerChannelOpt = Some(offsetManagerChannel)
-           queryChannel.disconnect()
-         }
-         catch {
-           case _: IOException => // offsets manager may have moved
-             info("Error while connecting to %s.".format(connectString))
-             if (offsetManagerChannel != null) offsetManagerChannel.disconnect()
-             Thread.sleep(retryBackOffMs)
-             offsetManagerChannelOpt = None // just in case someone decides to change shutdownChannel to not swallow exceptions
-         }
-       }
-     }
+      val coordinator = coordinatorOpt.get
+      if (coordinator.host == queryChannel.host && coordinator.port == queryChannel.port) {
+        offsetManagerChannelOpt = Some(queryChannel)
+      } else {
+        val connectString = "%s:%d".format(coordinator.host, coordinator.port)
+        var offsetManagerChannel: BlockingChannel = null
+        try {
+          debug("Connecting to offset manager %s.".format(connectString))
+          offsetManagerChannel = new BlockingChannel(coordinator.host, coordinator.port,
+            BlockingChannel.UseDefaultBufferSize,
+            BlockingChannel.UseDefaultBufferSize,
+            socketTimeoutMs)
+          offsetManagerChannel.connect()
+          offsetManagerChannelOpt = Some(offsetManagerChannel)
+          queryChannel.disconnect()
+        }
+        catch {
+          case _: IOException => // offsets manager may have moved
+            info("Error while connecting to %s.".format(connectString))
+            if (offsetManagerChannel != null) offsetManagerChannel.disconnect()
+            Thread.sleep(retryBackOffMs)
+            offsetManagerChannelOpt = None // just in case someone decides to change shutdownChannel to not swallow exceptions
+        }
+      }
+    }
 
-     offsetManagerChannelOpt.get
-   }
- }
+    offsetManagerChannelOpt.get
+  }
+}
